b853e4b9bdd8f3d1a54b10148359978b46ad7443
burytheflower <48148052+burytheflower@users.noreply.github.com>
2020-01-15 12:51:41 +0800

    优化代码

--- SwitchButton.hpp
+++ SwitchButton.hpp
@@ -16,18 +16,24 @@ public:
     static const QColor DEFAULT_OFF_BG_COLOR;
     static const QColor DEFAULT_ON_SLIDER_COLOR;
     static const QColor DEFAULT_OFF_SLIDER_COLOR;
+    static const QColor DEFAULT_DISABLED_ON_BG_COLOR;
+    static const QColor DEFAULT_DISABLED_OFF_BG_COLOR;
+    static const QColor DEFAULT_DISABLED_ON_SLIDER_COLOR;
+    static const QColor DEFAULT_DISABLED_OFF_SLIDER_COLOR;
 
-    // 设置背景颜色
+    // 背景颜色
     QColor onBgColor() const { return _onBgColor; }
     void setOnBgColor(const QColor &color) { _onBgColor = color; repaint(); }
     QColor offBgColor() const { return _offBgColor; }
     void setOffBgColor(const QColor &color) { _offBgColor = color; repaint(); }
 
-    // 设置滑块颜色
+    // 滑块颜色
     QColor onSliderColor() const { return _onSliderColor; }
     void setOnSliderColor(const QColor &color) { _onSliderColor = color; repaint(); }
     QColor offSliderColor() const { return _offSliderColor; }
     void setOffSliderColor(const QColor &color) { _offSliderColor = color; repaint(); }
+
+    virtual QSize sizeHint() const override { return QSize(fontMetrics().height() * 2, fontMetrics().height()); }
 
     /// 去除不需要的成员函数
     // 是否可选
@@ -36,8 +42,6 @@ public:
     // 文本
     QString text() const = delete;
     void setText(const QString &text) = delete;
-
-    virtual QSize sizeHint() const override { return QSize(fontMetrics().height() * 2, fontMetrics().height()); }
 
 signals:
     void checkStateChanged(bool checked);
@@ -51,9 +55,10 @@ private:
 
     void drawSlider(QPainter *painter, const QRectF &rect);
 
-    // 默认尺寸
-    static constexpr int DEFAULT_WIDTH {40};
-    static constexpr int DEFAULT_HEIGHT {20};
+    /// 暂时以字体尺寸作为默认尺寸
+//    // 默认尺寸
+//    static constexpr int DEFAULT_WIDTH {40};
+//    static constexpr int DEFAULT_HEIGHT {20};
 
     QColor _onBgColor {DEFAULT_ON_BG_COLOR};
     QColor _offBgColor {DEFAULT_OFF_BG_COLOR};
